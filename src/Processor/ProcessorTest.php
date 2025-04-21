LastDragon-ru/lara-asp-documentator

commit a1910029477aa94a7e5216e2c0815f0b793e9c58

Added `\LastDragon_ru\LaraASP\Documentator\Processor\Iterator` that allow add files into queue.

src/Processor/Executor.php
@@ -3,7 +3,6 @@
 namespace LastDragon_ru\LaraASP\Documentator\Processor;
 
 use Exception;
-use Iterator;
 use LastDragon_ru\LaraASP\Documentator\Processor\Contracts\Task;
 use LastDragon_ru\LaraASP\Documentator\Processor\Events\FileFinished;
 use LastDragon_ru\LaraASP\Documentator\Processor\Events\FileFinishedResult;
@@ -44,9 +43,6 @@ class Executor {
         private readonly Dispatcher $dispatcher,
         private readonly Tasks $tasks,
         private readonly FileSystem $fs,
-        /**
-         * @var Iterator<array-key, File>
-         */
         private readonly Iterator $iterator,
         private readonly Globs $exclude,
     ) {
@@ -56,11 +52,7 @@ class Executor {
     public function run(): void {
         $this->file($this->fs->getFile(Hook::Before));
 
-        while ($this->iterator->valid()) {
-            $file = $this->iterator->current();
-
-            $this->iterator->next();
-
+        foreach ($this->iterator as $file) {
             $this->file($file);
         }
 
@@ -171,7 +163,7 @@ class Executor {
         }
 
         // Process
-        if (!$isBeforeHook && $resolved instanceof FileReal && $file !== $resolved) {
+        if (!$isBeforeHook && $resolved instanceof FileReal) {
             $this->file($resolved);
         }
     }

src/Processor/ProcessorTest.php
@@ -605,9 +605,9 @@ final class ProcessorTest extends TestCase {
     }
 
     public function testRunCircularDependencySelf(): void {
-        $input = (new DirectoryPath(self::getTestData()->path('')))->getNormalizedPath();
+        $input = (new DirectoryPath(self::getTestData()->path('a/a')))->getNormalizedPath();
         $task  = new ProcessorTest__Task([
-            'c.txt' => ['c.txt'],
+            'aa.txt' => ['aa.txt'],
         ]);
 
         self::expectException(DependencyCircularDependency::class);
@@ -615,8 +615,8 @@ final class ProcessorTest extends TestCase {
             <<<MESSAGE
             Circular Dependency detected:
 
-            * {$input}/c.txt
-            ! {$input}/c.txt
+            * {$input}/aa.txt
+            ! {$input}/aa.txt
             MESSAGE,
         );
 

src/Processor/Iterator.php
@@ -1,0 +1,50 @@
+<?php declare(strict_types = 1);
+
+namespace LastDragon_ru\LaraASP\Documentator\Processor;
+
+use IteratorAggregate;
+use LastDragon_ru\LaraASP\Core\Path\FilePath;
+use LastDragon_ru\LaraASP\Documentator\Processor\FileSystem\File;
+use LastDragon_ru\LaraASP\Documentator\Processor\FileSystem\FileSystem;
+use Override;
+use Traversable;
+
+use function array_shift;
+
+/**
+ * @internal
+ * @implements IteratorAggregate<mixed, File>
+ */
+class Iterator implements IteratorAggregate {
+    /**
+     * @var array<mixed, FilePath>
+     */
+    private array $files = [];
+
+    public function __construct(
+        private readonly FileSystem $fs,
+        /**
+         * @var Traversable<mixed, File>
+         */
+        private readonly Traversable $iterator,
+    ) {
+        // empty
+    }
+
+    #[Override]
+    public function getIterator(): Traversable {
+        yield from [];
+        yield from $this->iterator;
+
+        while ($this->files !== []) {
+            $file = array_shift($this->files);
+            $file = $this->fs->getFile($file);
+
+            yield $file;
+        }
+    }
+
+    public function push(FilePath $file): void {
+        $this->files[] = $file;
+    }
+}

src/Processor/Processor.php
@@ -189,7 +189,7 @@ class Processor {
         ?int $depth,
     ): void {
         $filesystem = new FileSystem($this->dispatcher, $this->metadata, $input, $output, $this->consistent);
-        $iterator   = $filesystem->getFilesIterator($input, $include, $exclude, $depth);
+        $iterator   = new Iterator($filesystem, $filesystem->getFilesIterator($input, $include, $exclude, $depth));
         $executor   = new Executor($this->dispatcher, $this->tasks, $filesystem, $iterator, new Globs($exclude));
 
         $executor->run();

commit 4a2365a87a04e8dcec1e4ebb4b3e122202c53cbd

fix(documentator/Processor): Immediate self-references will not fail with "circular dependency" error.
Regression was introduced in a1910029477aa94a7e5216e2c0815f0b793e9c58.

src/Processor/Executor.php
@@ -169,7 +169,7 @@ class Executor {
         }
 
         // Process
-        if (!$isBeforeHook && $resolved instanceof FileReal) {
+        if (!$isBeforeHook && $file !== $resolved && $resolved instanceof FileReal) {
             $this->file($resolved);
         }
     }

src/Processor/ProcessorTest.php
@@ -646,12 +646,43 @@ final class ProcessorTest extends TestCase {
             'aa.txt' => ['aa.txt'],
         ]);
 
+        (new Processor($this->app()->make(ContainerResolver::class)))
+            ->addTask($task)
+            ->run($input);
+
+        self::assertEquals(
+            [
+                [
+                    (string) $input->getFilePath('aa.txt'),
+                    [
+                        'aa.txt' => (string) $input->getFilePath('aa.txt'),
+                    ],
+                ],
+                [
+                    (string) $input->getFilePath('excluded.txt'),
+                    [
+                        // empty
+                    ],
+                ],
+            ],
+            $task->processed,
+        );
+    }
+
+    public function testRunCircularDependencySelfThrough(): void {
+        $input = (new DirectoryPath(self::getTestData()->path('a/a')))->getNormalizedPath();
+        $task  = new ProcessorTest__Task([
+            'aa.txt'       => ['excluded.txt'],
+            'excluded.txt' => ['aa.txt'],
+        ]);
+
         self::expectException(DependencyCircularDependency::class);
         self::expectExceptionMessage(
             <<<MESSAGE
             Circular Dependency detected:
 
             * {$input}/aa.txt
+            * {$input}/excluded.txt
             ! {$input}/aa.txt
             MESSAGE,
         );